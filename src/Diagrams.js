ebdd62751bf4a9d97ee1887dc255c1a3e9b1c898
Rainer Winkler <rainer.winkler@poaceae.de>
2022-02-11 18:15:50 +0100

    #21 Start implementation
    There are still major problems with finding elements and positioning of elements does not work at all.

--- src/Diagrams.js
+++ src/Diagrams.js
@@ -223,3 +223,143 @@ async function SaveDisplayedDiagram() {
   await writable.close();
 
 }
+
+async function ImportOldDiagram() {
+
+  // Required to migrate Diagrams on FAMIX models to the new version of Moose2Model with SOMIX models
+
+  if (typeof workDirectoryHandle === 'undefined') {
+    window.alert("You have to specify a work directory first");
+    return;
+  }
+  const pickerOpts = {
+    types: [
+      {
+        description: 'Old Moose2Model diagram specifications',
+        accept: {
+          'application/xml': ['.xml']
+        }
+      },
+    ],
+    excludeAcceptAllOption: true,
+    multiple: false
+  };
+  let fileHandle;
+  [fileHandle] = await window.showOpenFilePicker(pickerOpts);
+  const file = await fileHandle.getFile();
+  const contents = await file.text();
+  const parser = new DOMParser();
+
+  const doc = parser.parseFromString(contents, "application/xml");
+  // print the name of the root element or error message
+  const errorNode = doc.querySelector("parsererror");
+  if (errorNode) {
+    console.log("error while parsing");
+    window.alert("Parsing failed");
+    return;
+  } else {
+    console.log(doc.documentElement.nodeName);
+  }
+
+  // --- Extract generation info from DOM model
+
+  // --- Change the displayed diagram and set it active so that it can be changed
+
+  // Remove the extension .m2m Name of file is equal to name of diagram
+
+  let fileName = file.name;
+  let len = fileName.length;
+  if (fileName.substring(len - 4, len) == '.xml') {
+    fileName = fileName.substring(0, len - 4);
+  }
+  newDiagram(fileName);
+  setDiagramActive(fileName);
+  switchDiagram(fileName);
+
+  doc.children[0].children
+
+  for (var step = 1; step <= 2; step++) {
+
+    for (const n of doc.children[0].children) {
+      if (n.tagName == 'scope') {
+        if (n.textContent != 'method') {
+          window.alert("Scope of diagram is not supported");
+          return;
+        }
+      }
+      if (n.tagName == 'element') {
+        let n_type = '';
+        let n_class = '';
+        let n_method = '';
+        let n_attribute = '';
+        let n_x = '';
+        let n_y = '';
+        let n_add_explicitly = false;
+        for (const e of n.children) {
+          if (e.tagName == 'type') {
+            n_type = e.textContent;
+          }
+          if (e.tagName == 'class') {
+            n_class = e.textContent;
+          }
+          if (e.tagName == 'method') {
+            n_method = e.textContent;
+          }
+          if (e.tagName == 'attribute') {
+            n_attribute = e.textContent;
+          }
+          if (e.tagName == 'x') {
+            n_x = e.textContent;
+          }
+          if (e.tagName == 'y') {
+            n_y = e.textContent;
+          }
+          if (e.tagName == 'ACTSuppressOthers') {
+            // Here it is assumed that there are no xml files where not the next neighbors are added explicitly
+            n_add_explicitly = true;
+          }
+        }
+
+        if (n_type == 'FAMIXMethod' || n_type == 'FAMIXAttribute') {
+          let expSOMIX = '';
+          let expName = '';
+          if (n_type == 'FAMIXMethod') {
+            expSOMIX = 'SOMIX.Code';
+            expName = n_method;
+          }
+          if (n_type == 'FAMIXAttribute') {
+            expSOMIX = 'SOMIX.Data';
+            expName = n_attribute;
+          }
+          for (const e2 of modelElementsByIndex) {
+            if (typeof e2 !== 'undefined') {
+              if (e2.element == expSOMIX && e2.name == expName) {
+                for (const pc of parentChildByChild[e2.index]) {
+                  let foundElement = modelElementsByIndex[pc.parent];
+                  if (foundElement.name == n_class) {
+                    if (step == 1) {
+                      // --- Add elements with neighbor to this diagram
+                      if (n_add_explicitly) {
+                        addWithNeighbors(foundElement);
+                      }
+                    }
+                    if (step == 2) {
+                      // --- Position elements on this diagram
+                      if (typeof diagramms[diagramInfos.activeDiagram].complModelPosition[foundElement.index] !== 'undefined') {
+                        diagramms[diagramInfos.activeDiagram].complModelPosition[foundElement.index].x = parseFloat(n_x);
+                        diagramms[diagramInfos.activeDiagram].complModelPosition[foundElement.index].y = parseFloat(n_y);
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+
+      }
+    }
+  }
+
+
+}
