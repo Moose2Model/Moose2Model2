1af421332e63b86a96d56095a2106723b7a103f8
Rainer Winkler <rainer.winkler@poaceae.de>
2022-05-15 09:20:47 +0200

    #37 New JavaScript File for logic of model explorer

--- src/Drawing.js
+++ src/Drawing.js
@@ -908,10 +908,7 @@ function draw(always = true) {
             }
         }
     } else {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.font = '15px sans-serif';
-        ctx.fillStyle = 'black'
-        ctx.fillText('The model explorer will be displayed here', 10, 50);
+        drawModelExplorer();
     }
     // }
 }
---
+++ src/ModelExplorer.js
@@ -0,0 +1,9 @@
+'use strict';
+function drawModelExplorer() {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.font = '15px sans-serif';
+    ctx.fillStyle = 'black'
+    ctx.fillText('The model explorer will be displayed here', 10, 50);
+
+}
