bbcd84f5c11cd5b2ba61d0991acee1c332f4792d
Rainer Winkler <rainer.winkler@poaceae.de>
2022-05-22 09:30:18 +0200

    #36 Place comments for grouping
    Adding comments for grouping worked already. With this change the comment is placed at the upper right corner of the box around all contained elements of the grouping.

--- src/Comment.js
+++ src/Comment.js
@@ -17,13 +17,26 @@ function comment(element) {
 
 function commentAdapted(text) {
     if (typeof diagramms[diagramInfos.activeDiagram].generationInfoInternal.commentsByID[lastElement.index] === 'undefined') {
+        let mebi = modelElementsByIndex[lastElement.index];
         if (text != '') {
-            diagramms[diagramInfos.activeDiagram].generationInfoInternal.commentsByID[lastElement.index] =
-            {
-                x: diagramms[diagramInfos.activeDiagram].complModelPosition[lastElement.index].x + defaultOffsetX,
-                y: diagramms[diagramInfos.activeDiagram].complModelPosition[lastElement.index].y + defaultOffsetY,
-                text: text
-            };
+            if (mebi.element == 'SOMIX.Code' || mebi.element == 'SOMIX.Data') {
+                diagramms[diagramInfos.activeDiagram].generationInfoInternal.commentsByID[lastElement.index] =
+                {
+                    x: diagramms[diagramInfos.activeDiagram].complModelPosition[lastElement.index].x + defaultOffsetX,
+                    y: diagramms[diagramInfos.activeDiagram].complModelPosition[lastElement.index].y + defaultOffsetY,
+                    text: text
+                };
+            } else {
+                diagramms[diagramInfos.activeDiagram].generationInfoInternal.commentsByID[lastElement.index] =
+                {
+                    // For groupings x and y are not really meaningful in diagrams. 
+                    // They make sense only in the force directed graph of all elements where they stand for the black dot.
+                    // So place the comment at the upper left corner of the current box around all contained elements of the grouping.
+                    x: diagramms[diagramInfos.activeDiagram].complModelPosition[lastElement.index].boxX1 + defaultOffsetX,
+                    y: diagramms[diagramInfos.activeDiagram].complModelPosition[lastElement.index].boxY1 + defaultOffsetY,
+                    text: text
+                };
+            }
         }
     } else {
         diagramms[diagramInfos.activeDiagram].generationInfoInternal.commentsByID[lastElement.index].text = text;
