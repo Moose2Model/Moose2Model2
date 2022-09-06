538bc38bcbde5f9cb87c84bb643015f381538643
Rainer Winkler <rainer.winkler@poaceae.de>
2022-09-06 21:00:47 +0200

    #44 Implement test coding to log files in selected directory to console

--- src/FileExtractorPreprocess.js
+++ src/FileExtractorPreprocess.js
@@ -1,8 +1,35 @@
 'use strict';
+
+// Example
+async function* getFilesRecursively(entry) {
+    if (entry.kind === 'file') {
+        const file = await entry.getFile();
+        if (file !== null) {
+            //file.relativePath = await directoryHandle.resolve(entry);
+            // file.relativePath = getRelativePath(entry);
+            //file.webkitRelativePath = getRelativePath(entry);
+
+            yield file;
+        }
+    } else if (entry.kind === 'directory') {
+        for await (const handle of entry.values()) {
+            yield* getFilesRecursively(handle);
+        }
+    }
+}
+
 async function SetExtractedFolder() {
     'use strict';
     try {
         FEDirectoryHandle = await window.showDirectoryPicker();
+
+        // Example
+        for await (const fileHandle of getFilesRecursively(FEDirectoryHandle)) {
+            console.log(fileHandle);
+        }
+
+
+
         // for await (const entry of workDirectoryHandle.values()) {
         //     console.log(entry.kind, entry.name);
         // }
