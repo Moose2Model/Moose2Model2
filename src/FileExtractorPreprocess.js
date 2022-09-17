2dcdecd8223b235662897ab21818a8f709a14949
Rainer Winkler <rainer.winkler@poaceae.de>
2022-09-17 21:41:20 +0200

    #44 Add logic to determine relative paths of files

--- src/FileExtractorPreprocess.js
+++ src/FileExtractorPreprocess.js
@@ -1,20 +1,30 @@
 'use strict';
 
+function getRelativePath(array) {
+    let path = '';
+    for (const a of array) {
+        path = path + '\\' + a;
+    }
+    return path;
+}
+
 // Example
-async function* getFilesRecursively(entry) {
+async function* getFilesRecursively(entry, directoryArray) {
     if (entry.kind === 'file') {
         const file = await entry.getFile();
         if (file !== null) {
             //file.relativePath = await directoryHandle.resolve(entry);
-            // file.relativePath = getRelativePath(entry);
+            file.relativePath = getRelativePath(directoryArray);
             //file.webkitRelativePath = getRelativePath(entry);
 
             yield file;
         }
     } else if (entry.kind === 'directory') {
+        directoryArray.push(entry.name);
         for await (const handle of entry.values()) {
-            yield* getFilesRecursively(handle);
+            yield* getFilesRecursively(handle, directoryArray);
         }
+        directoryArray.pop();
     }
 }
 
@@ -26,13 +36,26 @@ async function SetExtractedFolder() {
         FEDirectoryHandle = await window.showDirectoryPicker();
 
         // Example
-        for await (const fileHandle of getFilesRecursively(FEDirectoryHandle)) {
+        // See https://developer.mozilla.org/en-US/docs/Web/API/FileSystemDirectoryHandle/resolve
+        for await (const fileHandle of getFilesRecursively(FEDirectoryHandle, [])) {
             console.log(fileHandle);
-            const relativePaths = await FEDirectoryHandle.resolve(fileHandle);
-            for (const name of relativePaths) {
-                // log each entry
-                console.log(name);
-            }
+            /*             const relativePaths = await FEDirectoryHandle.resolve(fileHandle);
+            
+                        if (relativePaths === null) {
+                            // Not inside directory handle
+                          } else {
+                            // relativePath is an array of names, giving the relative path
+                        
+                            for (const name of relativePaths) {
+                              // log each entry
+                              console.log(name);
+                            }
+                          } */
+
+            /*             for (const name of relativePaths) {
+                            // log each entry
+                            console.log(name);
+                        } */
         }
 
 
