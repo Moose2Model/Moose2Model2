7e5256a8319e528ab11eefd0f0a76a828215c69a
Rainer Winkler <rainer.winkler@poaceae.de>
2022-09-20 21:46:53 +0200

    #44 A first draft of an SOMIX explorer for a file system

--- src/FileExtractorPreprocess.js
+++ src/FileExtractorPreprocess.js
@@ -42,6 +42,7 @@ async function* getFilesRecursively(fileInfo) {
         }
         fileInfo.directoryArray.pop();
     }
+
 }
 
 // Todo: 
@@ -55,10 +56,37 @@ async function* getFilesRecursively(fileInfo) {
 
 async function SetExtractedFolder() {
     'use strict';
-    try {
-        window.alert("This function is currently implemented. It will not either not work, or not work properly");
+    window.alert("This function is currently implemented. It will not either not work, or not work properly");
 
-        FEDirectoryHandle = await window.showDirectoryPicker();
+    FEDirectoryHandle = await window.showDirectoryPicker();
+
+    // From async function LoadModel() part 1
+
+
+    document.title = 'Testload from file';
+    loadModelText.innerHTML = 'Loaded SOMIX model: ' + document.title;
+    useStartDiagram();
+
+
+    try {
+
+        initializeBuildModel();
+        let elementName = '';
+        let idVal = 0;
+        let nameVal = '';
+        let uniqueNameVal = '';
+        let technicalTypeVal = '';
+        let linkToEditorVal = '';
+        let parentVal = 0;
+        let childVal = 0;
+        let isMainVal = false;
+        let callerVal = 0;
+        let calledVal = 0;
+        let accessorVal = 0;
+        let accessedVal = 0;
+        let isWriteVal = false;
+        let isReadVal = false;
+        let isDependentVal = false;
 
         let fileInfoStart = {};
         fileInfoStart.handle = FEDirectoryHandle;
@@ -76,9 +104,133 @@ async function SetExtractedFolder() {
             console.log(fileInfo.name);
             console.log(fileInfo.directoryArray);
             console.log(fileInfo.file);
+            if (fileInfo.handle.kind === 'file') {
+                elementName = 'SOMIX.Data';
+                idVal = fileInfo.index;
+                nameVal = fileInfo.name;
+                uniqueNameVal = '';
+                for (const e of fileInfo.directoryArray) {
+                    uniqueNameVal = uniqueNameVal + '.' + e;
+                }
+                uniqueNameVal = uniqueNameVal + '.' + fileInfo.name;
+                technicalTypeVal = 'File';
+                buildModel(
+                    elementName,
+                    idVal,
+                    nameVal,
+                    uniqueNameVal,
+                    technicalTypeVal,
+                    linkToEditorVal,
+                    parentVal,
+                    childVal,
+                    isMainVal,
+                    callerVal,
+                    calledVal,
+                    accessorVal,
+                    accessedVal,
+                    isWriteVal,
+                    isReadVal,
+                    isDependentVal);
+
+
+            } else if (fileInfo.handle.kind === 'directory') {
+                elementName = 'SOMIX.Grouping';
+                idVal = fileInfo.index;
+                nameVal = fileInfo.name;
+                uniqueNameVal = '';
+                for (const e of fileInfo.directoryArray) {
+                    uniqueNameVal = uniqueNameVal + '.' + e;
+                }
+                uniqueNameVal = uniqueNameVal + '.' + fileInfo.name;
+                technicalTypeVal = 'Directory';
+                buildModel(
+                    elementName,
+                    idVal,
+                    nameVal,
+                    uniqueNameVal,
+                    technicalTypeVal,
+                    linkToEditorVal,
+                    parentVal,
+                    childVal,
+                    isMainVal,
+                    callerVal,
+                    calledVal,
+                    accessorVal,
+                    accessedVal,
+                    isWriteVal,
+                    isReadVal,
+                    isDependentVal);
+
+            };
+
+            // Clear data before it is read again
+            elementName = '';
+            idVal = 0;
+            nameVal = '';
+            uniqueNameVal = '';
+            technicalTypeVal = '';
+            linkToEditorVal = '';
+            parentVal = '';
+            childVal = '';
+            isMainVal = false;
+            callerVal = 0;
+            calledVal = 0;
+            accessorVal = 0;
+            accessedVal = 0;
+            isWriteVal = false;
+            isReadVal = false;
+            isDependentVal = false;
+
+            elementName = 'SOMIX.ParentChild';
+            parentVal = fileInfo.parentIndex;
+            childVal = fileInfo.index;
+            isMainVal = true;
+            buildModel(
+                elementName,
+                idVal,
+                nameVal,
+                uniqueNameVal,
+                technicalTypeVal,
+                linkToEditorVal,
+                parentVal,
+                childVal,
+                isMainVal,
+                callerVal,
+                calledVal,
+                accessorVal,
+                accessedVal,
+                isWriteVal,
+                isReadVal,
+                isDependentVal);            
+
+            // Clear data before it is read again
+            elementName = '';
+            idVal = 0;
+            nameVal = '';
+            uniqueNameVal = '';
+            technicalTypeVal = '';
+            linkToEditorVal = '';
+            parentVal = '';
+            childVal = '';
+            isMainVal = false;
+            callerVal = 0;
+            calledVal = 0;
+            accessorVal = 0;
+            accessedVal = 0;
+            isWriteVal = false;
+            isReadVal = false;
+            isDependentVal = false;
+
         }
     } catch (err) {
         window.alert("Accessing a directory failed");
         return;
     }
+
+    // From async function LoadModel() part 2
+
+    positionCircle(g_width, g_height);
+    mouseover = true;
+    draw();
+
 }
