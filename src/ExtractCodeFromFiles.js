9c6ce934a728fb91206b7aa67be2d55c1cf3aba4
Rainer Winkler <rainer.winkler@poaceae.de>
2023-06-17 19:47:52 +0200

    #78 Prepare implementation of hoisting

--- src/ExtractCodeFromFiles.js
+++ src/ExtractCodeFromFiles.js
@@ -30,7 +30,7 @@ function javaScriptFindGlobal4(indexHTML, indexModel, codeParts) {
       // Known errors:
       // Thic code finds tokens in multiline comments
 
-      let tokens = codePart.code.match(/\/\/.*?$|:|[^\S\r\n]+|\r?\n|\/\*[\s\S]*?\*\/|([a-zA-Z_$][a-zA-Z0-9_$]*)|[\{\}]|[\(\)]|\b(let|const|function)\b/gm);
+      let tokens = codePart.code.match(/\/\/.*?$|:|[^\S\r\n]+|\r?\n|\/\*[\s\S]*?\*\/|(['"`])(.*?)\1|([a-zA-Z_$][a-zA-Z0-9_$]*)|[\{\}]|[\(\)]|\b(let|const|function)\b/gm);
 
 
       //   /\/\/.*?$       // Match single-line comments
@@ -42,6 +42,249 @@ function javaScriptFindGlobal4(indexHTML, indexModel, codeParts) {
       //   \r?\n          // Match newline characters
       //   |               // OR
       //   \/\*[\s\S]*?\*\/  // Match multiline comments
+      //   |               // OR
+      //      (['"`])(.*?)\1  // Matches strings enclosed in single quotes, double quotes, or b
+      //   |               // OR
+      //   ([a-zA-Z_$][a-zA-Z0-9_$]*)  // Match variables or identifiers
+      //   |               // OR
+      //   [\{\}]          // Match curly brackets
+      //   |               // OR
+      //   [\(\)]          // Match parentheses
+      //   |               // OR
+      //   \b(let|const|function)\b  // Match keywords (let, const, function)
+      // /gm);
+
+
+      if (tokens) {
+        tokens.forEach((token, index) => { // tokens.forEach
+
+          if (token === '(') {
+            braketLevel += 1;
+          }
+          if (token === ')') {
+            braketLevel -= 1; // TODO check for negative values 
+            if (braketLevel < 0) {
+              braketLevel = 0;
+            }
+          }
+
+          if (token === '{') {
+            braketLevel = 0;
+          }
+          if (token === '}') {
+            braketLevel = 0;
+          }
+
+          if (skipCount > 0) {
+            skipCount -= 1;
+            return;
+          }
+
+          else if (token === 'function') {
+            let nextToken = tokens[index + 1];
+            skipCount = 1;
+            if (/^\s*$/.test(nextToken)) {
+              nextToken = tokens[index + 2];
+              skipCount = 2;
+            }
+            if (/^[a-zA-Z_$]/.test(nextToken)) {
+              if (level == 0) {
+                currentFunction = nextToken;
+                currentFunctionIndex = indexModel;
+                currentFunctionContainer.currentFunction = currentFunction;
+                currentFunctionContainer.currentFunctionIndex = currentFunctionIndex;
+                functions[currentFunction] = {
+                  index: indexModel,
+                  container: codePart.container,
+                  used: []
+                };
+                indexModel += 1;
+              }
+            }
+          } else if (/^[a-zA-Z_$]/.test(token)) {
+            let nextToken = tokens[index + 1];
+            let nextToken2 = tokens[index + 2];
+            if (nextToken === '(') {
+              // Create a copy of the original object
+              var copiedObject = Object.assign({}, currentFunctionContainer);
+              functions[token] && functions[token].used && functions[token].used.push(copiedObject);
+            } else if (nextToken === ':' || (/^\s*$/.test(nextToken) && nextToken2 === ':')) // Check for '{ x:' or '{ x :' these are no references to variables
+            {
+              // ignore this is a specification for a class member
+            }
+            else {
+              const reservedWords = [
+                'abstract', 'await', 'boolean', 'break', 'byte', 'case', 'catch', 'char', 'class', 'const',
+                'continue', 'debugger', 'default', 'delete', 'do', 'double', 'else', 'enum', 'export', 'extends',
+                'false', 'final', 'finally', 'float', 'for', 'function', 'goto', 'if', 'implements', 'import',
+                'in', 'instanceof', 'int', 'interface', 'let', 'long', 'native', 'new', 'null', 'package', 'private',
+                'protected', 'public', 'return', 'short', 'static', 'super', 'switch', 'synchronized', 'this', 'throw',
+                'throws', 'transient', 'true', 'try', 'typeof', 'var', 'void', 'volatile', 'while', 'with', 'yield'
+                // exclude also constructor
+                , 'constructor'
+              ];
+
+              // if (token === 'let' || token === 'const' || token === 'constructor') {
+              if (reservedWords.includes(token)) {
+                // ignore
+              } else {
+                if (level == 0) {
+                  let isExistingVariable = false;
+                  if (variables[token]) {
+                    isExistingVariable = true;
+                  }
+                  const variable = variables[token] || {
+                    index: indexModel,
+                    used: []
+                  };
+                  if (!isExistingVariable) {
+                    indexModel += 1;
+                    variable.container = codePart.container;
+                  }
+                  if (isExistingVariable) {
+                    var copiedObject = Object.assign({}, currentFunctionContainer);
+                    variable.used.push(copiedObject);
+                  }
+                  if ((!isExistingVariable && braketLevel == 0) || isExistingVariable) {
+                    variables[token] = variable;
+                  }
+                }
+                else {
+                  if (typeof variables[token] !== 'undefined') {
+                    const variable = variables[token]
+                    // 17.06.2023 This was apparently wrong and caused the container to be overwritten
+                    //variable.container = codePart.container;
+                    var copiedObject = Object.assign({}, currentFunctionContainer);
+                    variable.used && variable.used.push(copiedObject);
+                    variables[token] = variable;
+                  }
+                }
+              }
+            }
+          } else if (token === '{') {
+            level += 1;
+          } else if (token === '}') {
+
+            if (level == 1) {
+              if (currentFunction !== '') {
+                currentFunction = '';
+                currentFunctionIndex = indexHTML;
+                currentFunctionContainer = {};
+                currentFunctionContainer.currentFunction = currentFunction;
+                currentFunctionContainer.currentFunctionIndex = currentFunctionIndex;
+              }
+            }
+            level -= 1;
+          }
+        } // END tokens.forEach
+
+        );
+      }
+    }
+  } // END for (const codePart of codeParts)
+  let result = {};
+  for (let v in variables) {
+    // (variables[v].used) && variables[v].used.sort();
+    // (variables[v].used) && (variables[v].used = [...new Set(variables[v].used)]); // Remove duplicates
+
+
+    // Sort the array by currentFunctionIndex
+    (variables[v].used) && variables[v].used.sort(function (a, b) {
+      return a.currentFunctionIndex - b.currentFunctionIndex;
+    });
+
+    // Remove duplicate elements
+    (variables[v].used) && (variables[v].used = variables[v].used.filter(function (obj, index, self) {
+      return (
+        index ===
+        self.findIndex(function (o) {
+          return (
+            o.currentFunction === obj.currentFunction &&
+            o.currentFunctionIndex === obj.currentFunctionIndex
+          );
+        })
+      );
+    }));
+
+
+
+
+
+  }
+  for (let f in functions) {
+    // (functions[f].used) && functions[f].used.sort();
+    // (functions[f].used) && (functions[f].used = [...new Set(functions[f].used)]); // Remove duplicates
+
+    // Sort the array by currentFunctionIndex
+    (functions[f].used) && functions[f].used.sort(function (a, b) {
+      return a.currentFunctionIndex - b.currentFunctionIndex;
+    });
+
+    // Remove duplicate elements
+    (functions[f].used) && (functions[f].used = functions[f].used.filter(function (obj, index, self) {
+      return (
+        index ===
+        self.findIndex(function (o) {
+          return (
+            o.currentFunction === obj.currentFunction &&
+            o.currentFunctionIndex === obj.currentFunctionIndex
+          );
+        })
+      );
+    }));
+
+
+
+
+
+
+
+
+  }
+  result.variables = variables;
+  result.functions = functions;
+  result.index = indexModel;
+  return result;
+}
+
+function javaScriptFindGlobal5(indexHTML, indexModel, codeParts) {
+
+  // This is currently a draft
+  // Shadowing by local variables and functions is not handled
+  // Ambiguity due to variables and functions with the same name is not needed to be handled. This appears to forbidden in strict mode. And otherwise fucntions overwrite the variable.
+
+  const variables = {};
+  const functions = {};
+
+  let currentFunction = '';
+  let currentFunctionIndex = indexHTML;
+  let currentFunctionContainer = {};
+  currentFunctionContainer.currentFunction = currentFunction;
+  currentFunctionContainer.currentFunctionIndex = currentFunctionIndex;
+
+  let skipCount = 0;
+  let level = 0;
+  let braketLevel = 0;
+  for (const codePart of codeParts) { // for (const codePart of codeParts)
+    if (codePart.code) {
+
+      // Known errors:
+      // Thic code finds tokens in multiline comments
+
+      let tokens = codePart.code.match(/\/\/.*?$|:|[^\S\r\n]+|\r?\n|\/\*[\s\S]*?\*\/|(['"`])(.*?)\1|([a-zA-Z_$][a-zA-Z0-9_$]*)|[\{\}]|[\(\)]|\b(let|const|function)\b/gm);
+
+
+      //   /\/\/.*?$       // Match single-line comments
+      //   |               // OR
+      //   :               // Match colon symbol
+      //   |               // OR
+      //   [^\S\r\n]+      // Match whitespace characters except newlines
+      //   |               // OR
+      //   \r?\n          // Match newline characters
+      //   |               // OR
+      //   \/\*[\s\S]*?\*\/  // Match multiline comments
+      //   |               // OR
+      //      (['"`])(.*?)\1  // Matches strings enclosed in single quotes, double quotes, or b
       //   |               // OR
       //   ([a-zA-Z_$][a-zA-Z0-9_$]*)  // Match variables or identifiers
       //   |               // OR
@@ -1053,19 +1296,43 @@ function testFindGlobal4() {
 
 function testFindGlobal5() {
   const jsCode = `
-  let modelElementsByUniqueKey = {};
+    let x = 42;
+    const y = x;
+    const cl = {xp: 2};
+    // const yc = 3.14;
+    /* 
+    const yd = 3.14; 
+    */
+    function foo(para1) {
+      let z = 100;
+      console.log(x + y + z);
+      function subfoo(){
+
+      }
+      subfoo(){
+        y = 3;
+      };
+    }
+    foo();
+    function foo2(){
+      foo();
+      y = 1; 
+      const cl = {x: 2};
+      const cl = {x : 2};
+      foo();
+    }
   `;
-  const jsCode2 = `
-  if (typeof modelElementsByUniqueKey[e] !== 'undefined') {}
+  const jsCode2 = `foo();
+  let z1 = 42;
  `;
 
   const jsCodes = [{ container: 'First', code: jsCode }, { container: 'Second', code: jsCode2 }]
 
-  const result = javaScriptFindGlobal4(1, 2, jsCodes);
+  const result = javaScriptFindGlobal5(1, 2, jsCodes);
 
-  console.log('5 Variables:', result.variables);
-  console.log('5 Functions:', result.functions);
-  console.log('5 Index after analysis:', result.index);
+  console.log('Variables:', result.variables);
+  console.log('Functions:', result.functions);
+  console.log('Index after analysis:', result.index);
 
   const variablesExp = {
     x: {
@@ -1075,19 +1342,19 @@ function testFindGlobal5() {
     },
     y: {
       index: 3, used: [{ currentFunction: 'foo', currentFunctionIndex: 5 },
-      { currentFunction: 'foo2', currentFunctionIndex: 6 }], container: 'First'
+      { currentFunction: 'foo2', currentFunctionIndex: 7 }], container: 'First'
     },
     cl: {
       index: 4, used: [
-        { currentFunction: 'foo2', currentFunctionIndex: 6 }], container: 'First'
+        { currentFunction: 'foo2', currentFunctionIndex: 7 }], container: 'First'
     },
-    z1: { index: 7, used: [], container: 'Second' }
+    z1: { index: 8, used: [], container: 'Second' }
   };
   const functionsExp = {
-    foo: { index: 5, container: 'First', used: [{ currentFunction: '', currentFunctionIndex: 1 }, { currentFunction: 'foo2', currentFunctionIndex: 6 }] },
-    foo2: { index: 6, container: 'First', used: [] }
+    foo: { index: 5, container: 'First', used: [{ currentFunction: '', currentFunctionIndex: 1 }, { currentFunction: 'foo2', currentFunctionIndex: 7 }] },
+    foo2: { index: 7, container: 'First', used: [] }
   };
-  const indexExp = 8;
+  const indexExp = 9;
 
   console.log('VariablesExp:', variablesExp);
   console.log('FunctionsExp:', functionsExp);
@@ -1113,3 +1380,4 @@ function testFindGlobal5() {
 
 
 }
+
---
+++ src/FileExtractorPreprocess.js
@@ -0,0 +1,323 @@
+
+'use strict';
+
+function getRelativePath(array) {
+    let path = '';
+    for (const a of array) {
+        path = path + '\\' + a;
+    }
+    return path;
+}
+// Add comment to the next variable
+/**  Points to the next unused index in a model */
+let gIndex;
+
+async function* getFilesRecursively(fileInfo) {
+    let parentIndex = fileInfo.parentIndex;
+    if (fileInfo.handle.kind === 'file') {
+        try {
+            // fileInfo.file = await fileInfo.handle.getFile();
+        } catch (err2) {
+            window.alert("Getting files failed");
+            return;
+        }
+        //if (fileInfo.file !== null) {
+        //fileInfo.file.relativePath = getRelativePath(fileInfo.directoryArray);
+        fileInfo.parentIndex = parentIndex;
+        fileInfo.index = gIndex;
+        gIndex += 1;
+        yield fileInfo;
+        //}
+    } else if (fileInfo.handle.kind === 'directory') {
+        if (ignoreDotGitFolder && fileInfo.name != '.git') {
+            fileInfo.directoryArray.push(fileInfo.handle.name);
+            fileInfo.parentIndex = parentIndex;
+            fileInfo.index = gIndex;
+            gIndex += 1;
+            yield fileInfo;
+            for await (const handle of fileInfo.handle.values()) {
+                let fileInfoNewDirectory = {};
+                fileInfoNewDirectory.handle = handle;
+                fileInfoNewDirectory.index = gIndex;
+                //gIndex += 1;
+                fileInfoNewDirectory.parentIndex = fileInfo.index;
+                fileInfoNewDirectory.kind = handle.kind;
+                fileInfoNewDirectory.name = handle.name;
+                fileInfoNewDirectory.directoryArray = fileInfo.directoryArray;
+                fileInfoNewDirectory.file = {};
+                //yield fileInfoNewDirectory;
+                yield* getFilesRecursively(fileInfoNewDirectory);
+            }
+        }
+        fileInfo.directoryArray.pop();
+    }
+
+}
+
+// Todo: 
+// Do not list only files, but files and directories.
+// fileInfo with therefore contains file system elements. 
+// Which are either: Directories - Mapped to SOMIX Groupings
+//                   Files - Mapped to SOMIX Groupings
+// The code (and data) of files is then a children of the SOMIX Groupings of the file
+// A directory which contains further directory is modelled as SOMIX Groupings where one is the parent of the other one
+// This has to be specified in a separate document where the handling of files in SOMIX is specified.
+
+async function SetExtractedFolder() {
+    'use strict';
+    // window.alert("This function is currently implemented. It will not either not work, or not work properly");
+
+    FEDirectoryHandle = await window.showDirectoryPicker();
+
+    // From async function LoadModel() part 1
+
+
+    document.title = FEDirectoryHandle.name;
+    loadModelText.innerHTML = 'Loaded SOMIX model (File System): ' + document.title;
+    useStartDiagram();
+
+
+    try {
+
+        initializeBuildModel();
+        let elementName = '';
+        let idVal = 0;
+        let nameVal = '';
+        let uniqueNameVal = '';
+        let technicalTypeVal = '';
+        let linkToEditorVal = '';
+        let parentVal = 0;
+        let childVal = 0;
+        let isMainVal = false;
+        let callerVal = 0;
+        let calledVal = 0;
+        let accessorVal = 0;
+        let accessedVal = 0;
+        let isWriteVal = false;
+        let isReadVal = false;
+        let isDependentVal = false;
+
+        let fileInfoStart = {};
+        fileInfoStart.handle = FEDirectoryHandle;
+        fileInfoStart.index = 0;
+        gIndex = 1;
+        fileInfoStart.parentIndex = 0.
+        fileInfoStart.kind = FEDirectoryHandle.kind;
+        fileInfoStart.name = FEDirectoryHandle.name;
+        fileInfoStart.directoryArray = [];
+        fileInfoStart.file = {};
+        for await (const fileInfo of getFilesRecursively(fileInfoStart)) {
+            // console.log(fileInfo.kind);
+            // console.log(fileInfo.index);
+            // console.log(fileInfo.parentIndex);
+            // console.log(fileInfo.name);
+            // console.log(fileInfo.directoryArray);
+            //console.log(fileInfo.file);
+            if (fileInfo.handle.kind === 'file') {
+                elementName = 'SOMIX.Data';
+                idVal = fileInfo.index;
+                nameVal = fileInfo.name;
+                uniqueNameVal = '';
+                for (const e of fileInfo.directoryArray) {
+                    uniqueNameVal = uniqueNameVal + '/' + e;
+                }
+                uniqueNameVal = uniqueNameVal + '/' + fileInfo.name;
+                technicalTypeVal = 'File';
+                buildModel(
+                    elementName,
+                    idVal,
+                    nameVal,
+                    uniqueNameVal,
+                    technicalTypeVal,
+                    linkToEditorVal,
+                    parentVal,
+                    childVal,
+                    isMainVal,
+                    callerVal,
+                    calledVal,
+                    accessorVal,
+                    accessedVal,
+                    isWriteVal,
+                    isReadVal,
+                    isDependentVal);
+
+
+            } else if (fileInfo.handle.kind === 'directory') {
+                elementName = 'SOMIX.Grouping';
+                idVal = fileInfo.index;
+                nameVal = fileInfo.name;
+                uniqueNameVal = '';
+                for (const e of fileInfo.directoryArray) {
+                    uniqueNameVal = uniqueNameVal + '/' + e;
+                }
+                // uniqueNameVal = uniqueNameVal + '/' + fileInfo.name;
+                technicalTypeVal = 'Folder';
+                buildModel(
+                    elementName,
+                    idVal,
+                    nameVal,
+                    uniqueNameVal,
+                    technicalTypeVal,
+                    linkToEditorVal,
+                    parentVal,
+                    childVal,
+                    isMainVal,
+                    callerVal,
+                    calledVal,
+                    accessorVal,
+                    accessedVal,
+                    isWriteVal,
+                    isReadVal,
+                    isDependentVal);
+
+            };
+
+            // Clear data before it is read again
+            elementName = '';
+            idVal = 0;
+            nameVal = '';
+            uniqueNameVal = '';
+            technicalTypeVal = '';
+            linkToEditorVal = '';
+            parentVal = '';
+            childVal = '';
+            isMainVal = false;
+            callerVal = 0;
+            calledVal = 0;
+            accessorVal = 0;
+            accessedVal = 0;
+            isWriteVal = false;
+            isReadVal = false;
+            isDependentVal = false;
+
+            elementName = 'SOMIX.ParentChild';
+            parentVal = fileInfo.parentIndex;
+            childVal = fileInfo.index;
+            isMainVal = true;
+            buildModel(
+                elementName,
+                idVal,
+                nameVal,
+                uniqueNameVal,
+                technicalTypeVal,
+                linkToEditorVal,
+                parentVal,
+                childVal,
+                isMainVal,
+                callerVal,
+                calledVal,
+                accessorVal,
+                accessedVal,
+                isWriteVal,
+                isReadVal,
+                isDependentVal);
+
+            // Clear data before it is read again
+            elementName = '';
+            idVal = 0;
+            nameVal = '';
+            uniqueNameVal = '';
+            technicalTypeVal = '';
+            linkToEditorVal = '';
+            parentVal = '';
+            childVal = '';
+            isMainVal = false;
+            callerVal = 0;
+            calledVal = 0;
+            accessorVal = 0;
+            accessedVal = 0;
+            isWriteVal = false;
+            isReadVal = false;
+            isDependentVal = false;
+
+        }
+    } catch (err) {
+        window.alert("Accessing a directory failed");
+        return;
+    }
+
+    // From async function LoadModel() part 2
+
+    positionCircle(g_width, g_height);
+    mouseover = true;
+    draw();
+    ZoomToFit();
+}
+
+
+
+async function ExtractCodeFromFolder() {
+    'use strict';
+    //window.alert("This function is currently implemented. It will either not work, or not work properly");
+
+    FEDirectoryHandle = await window.showDirectoryPicker();
+
+    // From async function LoadModel() part 1
+
+
+    document.title = FEDirectoryHandle.name;
+    loadModelText.innerHTML = 'Loaded SOMIX model (File System): ' + document.title;
+    useStartDiagram();
+
+
+
+
+    /*     initializeBuildModel();
+        let elementName = '';
+        let idVal = 0;
+        let nameVal = '';
+        let uniqueNameVal = '';
+        let technicalTypeVal = '';
+        let linkToEditorVal = '';
+        let parentVal = 0;
+        let childVal = 0;
+        let isMainVal = false;
+        let callerVal = 0;
+        let calledVal = 0;
+        let accessorVal = 0;
+        let accessedVal = 0;
+        let isWriteVal = false;
+        let isReadVal = false;
+        let isDependentVal = false; */
+
+    let fileInfoStart = {};
+    fileInfoStart.handle = FEDirectoryHandle;
+    fileInfoStart.index = 0;
+    gIndex = 1;
+    fileInfoStart.parentIndex = 0.
+    fileInfoStart.kind = FEDirectoryHandle.kind;
+    fileInfoStart.name = FEDirectoryHandle.name;
+    fileInfoStart.directoryArray = [];
+    fileInfoStart.file = {};
+    for await (const fileInfo of getFilesRecursively(fileInfoStart)) {
+
+        fileInfoByIndex[fileInfo.index] = {};
+        fileInfoByIndex[fileInfo.index].handle = fileInfo.handle;
+        fileInfoByIndex[fileInfo.index].index = fileInfo.index;
+        fileInfoByIndex[fileInfo.index].parentIndex = fileInfo.parentIndex;
+        fileInfoByIndex[fileInfo.index].kind = fileInfo.kind;
+        fileInfoByIndex[fileInfo.index].name = fileInfo.name;
+        fileInfoByIndex[fileInfo.index].extension = fileInfo.name.split('.').pop().toLowerCase();
+        fileInfoByIndex[fileInfo.index].file = {};
+        fileInfoByIndex[fileInfo.index].directoryArray = [];
+
+        if (fileInfo.handle.kind === 'file') {
+            fileInfoByIndex[fileInfo.index].file = await fileInfo.handle.getFile();
+        }
+
+        for (const d of fileInfo.directoryArray) {
+            fileInfoByIndex[fileInfo.index].directoryArray.push(d);
+        }
+
+    }
+
+    await AnalyzeFileAndFolder();
+
+    // From async function LoadModel() part 2
+
+    positionCircle(g_width, g_height);
+    mouseover = true;
+    draw();
+    ZoomToFit();
+
+}
