546ecd24b6ed7104fec752cb8e16fd4e45bab227
Rainer Winkler <rainer.winkler@poaceae.de>
2023-10-13 17:46:03 +0200

    Load the most recent SOMIX file automatically when it exists #93

--- src/HTMLLogic.js
+++ src/HTMLLogic.js
@@ -22,24 +22,25 @@ else {
 
 // startLogic();
 
-async function LoadModel() {
+async function LoadModel(fileHandle) {
     'use strict';
     let contents;
     // open file picker
-    try {
-        [fileHandle] = await window.showOpenFilePicker();
+    if (!fileHandle) {
+        try {
+            [fileHandle] = await window.showOpenFilePicker();
 
 
-    } catch (err) {
-        if (err.message == 'The user aborted a request.') { // TODO improve
-            // Nothing is to be done
-            return;
-        } else {
-            window.alert("Loading from file is not supported. This application can therefore not be used on this browser.");
-            return;
+        } catch (err) {
+            if (err.message == 'The user aborted a request.') { // TODO improve
+                // Nothing is to be done
+                return;
+            } else {
+                window.alert("Loading from file is not supported. This application can therefore not be used on this browser.");
+                return;
+            }
         }
     }
-
 
     if (fileHandle.kind === 'file') {
         // run file code
@@ -61,18 +62,62 @@ async function LoadModel() {
 
 }
 
+async function getMostRecentMseFile(workDirectoryHandle) {
+    try {
+
+        // Check if SOMIX folder exists in the working directory
+        const somixHandle = await workDirectoryHandle.getDirectoryHandle("SOMIX", { create: false });
+
+        // Get all the files in the SOMIX directory
+        const mseFiles = [];
+        for await (const entry of somixHandle.values()) {
+            if (entry.kind === 'file' && entry.name.endsWith('.mse')) {
+                mseFiles.push(entry);
+            }
+        }
+
+        // Check if there's at least one .mse file
+        if (mseFiles.length === 0) {
+            console.log("No .mse files found in SOMIX folder.");
+            return null;
+        }
+
+        // Find the most recent .mse file based on last modified date
+        let mostRecentFileHandle = mseFiles[0];
+        let mostRecentFileDate = await getFileLastModifiedDate(mostRecentFileHandle);
+
+        for (const fileHandle of mseFiles) {
+            const fileDate = await getFileLastModifiedDate(fileHandle);
+            if (fileDate > mostRecentFileDate) {
+                mostRecentFileDate = fileDate;
+                mostRecentFileHandle = fileHandle;
+            }
+        }
+
+        return mostRecentFileHandle;
+    } catch (error) {
+        // This is not an error, it just means that the SOMIX folder doesn't exist
+        return null;
+    }
+}
+
+async function getFileLastModifiedDate(fileHandle) {
+    const file = await fileHandle.getFile();
+    return file.lastModified;
+}
+
 async function SetWorkFolder() {
     'use strict';
     try {
         workDirectoryHandle = await window.showDirectoryPicker();
-        // for await (const entry of workDirectoryHandle.values()) {
-        //     console.log(entry.kind, entry.name);
-        // }
-        // await workDirectoryHandle.removeEntry('d2.txt');
-        // console.log('After removing');
-        // for await (const entry of workDirectoryHandle.values()) {
-        //     console.log(entry.kind, entry.name);
-        // }
+
+        if (workDirectoryHandle) {
+            let mseFileHandle = await getMostRecentMseFile(workDirectoryHandle);
+            if (mseFileHandle) {
+                LoadModel(mseFileHandle);
+            }
+        }
+
     } catch (err) {
         window.alert("This application is not supported in your browser. Please use a different browser.");
         return;
